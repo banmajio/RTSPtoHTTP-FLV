6bf1c7b752b8d6b0fbf00d6eec7d5e8d7280d78c
banmajio <53471385+banmajio@users.noreply.github.com>
2023-08-30 09:42:24 +0800

    使用lombok

--- src/main/java/com/junction/pojo/Config.java
+++ src/main/java/com/junction/pojo/Config.java
@@ -1,86 +1,60 @@
 package com.junction.pojo;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
+ * @author banmajio
  * @Title ConfigPojo.java
  * @description 读取配置文件的bean
  * @time 2019年12月25日 下午5:11:21
- * @author wuguodong
  **/
 @Component
 @ConfigurationProperties(prefix = "config")
+@Data
 public class Config {
-	private String keepalive;// 保活时长（分钟）
-	private String push_host;// 推送地址
-	private String host_extra;// 额外地址
-	private String push_port;// 推送端口
-	private String main_code;// 主码流最大码率
-	private String sub_code;// 主码流最大码率
-	private String version;// 版本信息
-
-	public String getHost_extra() {
-		return host_extra;
-	}
-
-	public void setHost_extra(String host_extra) {
-		this.host_extra = host_extra;
-	}
-
-	public String getKeepalive() {
-		return keepalive;
-	}
-
-	public void setKeepalive(String keepalive) {
-		this.keepalive = keepalive;
-	}
-
-	public String getPush_host() {
-		return push_host;
-	}
-
-	public void setPush_host(String push_host) {
-		this.push_host = push_host;
-	}
-
-	public String getPush_port() {
-		return push_port;
-	}
-
-	public void setPush_port(String push_port) {
-		this.push_port = push_port;
-	}
-
-	public String getMain_code() {
-		return main_code;
-	}
-
-	public void setMain_code(String main_code) {
-		this.main_code = main_code;
-	}
-
-	public String getSub_code() {
-		return sub_code;
-	}
-
-	public void setSub_code(String sub_code) {
-		this.sub_code = sub_code;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
-	}
-
-	@Override
-	public String toString() {
-		return "Config [keepalive=" + keepalive + ", push_host=" + push_host + ", host_extra=" + host_extra
-				+ ", push_port=" + push_port + ", main_code=" + main_code + ", sub_code=" + sub_code + ", version="
-				+ version + "]";
-	}
+    /**
+     * @description: 保活时长（分钟）
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String keepalive;
+    /**
+     * @description: 推送地址
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String pushHost;
+    /**
+     * @description: 额外地址
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String hostExtra;
+    /**
+     * @description: 推送端口
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String pushPort;
+    /**
+     * @description: 主码流最大码率
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String mainCode;
+    /**
+     * @description: 主码流最大码率
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String subCode;
+    /**
+     * @description: 版本信息
+     * @author: banmajio
+     * @date: 2023/8/30 09:26
+     */
+    private String version;
 
 }
