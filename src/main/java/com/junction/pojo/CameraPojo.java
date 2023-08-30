3ba0104db763d522ef6e413ec77c5891e9f6bb33
banmajio <53471385+banmajio@users.noreply.github.com>
2023-08-30 09:26:22 +0800

    使用lombok

--- src/main/java/com/junction/pojo/CameraPojo.java
+++ src/main/java/com/junction/pojo/CameraPojo.java
@@ -1,133 +1,88 @@
 package com.junction.pojo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class CameraPojo implements Serializable {
-	private static final long serialVersionUID = 8183688502930584159L;
-	private String username;// 摄像头账号
-	private String password;// 摄像头密码
-	private String ip;// 摄像头ip
-	private String channel;// 摄像头通道
-	private String stream;// 摄像头码流
-	private String rtsp;// rtsp地址
-	private String rtmp;// rtmp地址
-	private String url;// 播放地址
-	private String starttime;// 回放开始时间
-	private String endtime;// 回放结束时间
-	private String opentime;// 打开时间
-	private int count = 0;// 使用人数
-	private String token;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getIp() {
-		return ip;
-	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-
-	public String getChannel() {
-		return channel;
-	}
-
-	public void setChannel(String channel) {
-		this.channel = channel;
-	}
-
-	public String getStream() {
-		return stream;
-	}
-
-	public void setStream(String stream) {
-		this.stream = stream;
-	}
-
-	public String getRtsp() {
-		return rtsp;
-	}
-
-	public void setRtsp(String rtsp) {
-		this.rtsp = rtsp;
-	}
-
-	public String getRtmp() {
-		return rtmp;
-	}
-
-	public void setRtmp(String rtmp) {
-		this.rtmp = rtmp;
-	}
-
-	public String getStarttime() {
-		return starttime;
-	}
-
-	public void setStarttime(String starttime) {
-		this.starttime = starttime;
-	}
-
-	public String getEndtime() {
-		return endtime;
-	}
-
-	public void setEndTime(String endtime) {
-		this.endtime = endtime;
-	}
-
-	public String getOpentime() {
-		return opentime;
-	}
-
-	public void setOpentime(String opentime) {
-		this.opentime = opentime;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	@Override
-	public String toString() {
-		return "CameraPojo [username=" + username + ", password=" + password + ", ip=" + ip + ", channel=" + channel
-				+ ", stream=" + stream + ", rtsp=" + rtsp + ", rtmp=" + rtmp + ", url=" + url + ", starttime="
-				+ starttime + ", endtime=" + endtime + ", opentime=" + opentime + ", count=" + count + ", token="
-				+ token + "]";
-	}
-
+    private static final long serialVersionUID = 8183688502930584159L;
+    /**
+     * @description: 摄像头账号
+     * @author: banmajio
+     * @date: 09:20
+     */
+    private String username;
+    /**
+     * @description: 摄像头密码
+     * @author: banmajio
+     * @date: 2023/8/30 09:21
+     */
+    private String password;
+    /**
+     * @description: 摄像头ip
+     * @author: banmajio
+     * @date: 2023/8/30 09:21
+     */
+    private String ip;
+    /**
+     * @description: 摄像头通道
+     * @author: banmajio
+     * @date: 2023/8/30 09:21
+     */
+    private String channel;
+    /**
+     * @description: 摄像头码流
+     * @author: banmajio
+     * @date: 2023/8/30 09:21
+     */
+    private String stream;
+    /**
+     * @description: rtsp地址
+     * @author: banmajio
+     * @date: 2023/8/30 09:21
+     */
+    private String rtsp;
+    /**
+     * @description: rtmp地址
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String rtmp;
+    /**
+     * @description: 播放地址
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String url;
+    /**
+     * @description: 回放开始时间
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String starttime;
+    /**
+     * @description: 回放结束时间
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String endtime;
+    /**
+     * @description: 打开时间
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String opentime;
+    /**
+     * @description: 使用人数
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private int count = 0;
+    /**
+     * @description: token
+     * @author: banmajio
+     * @date: 2023/8/30 09:22
+     */
+    private String token;
 }
